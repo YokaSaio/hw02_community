e78c8e147218f98bdb0348d1b8ad1bb0b2abe9ca
Сергей Владимиров <goldremt@yandex.ru>
2023-01-13 17:17:52 +0300

    Исправление ошибки 4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POST_COUNT]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POST_COUNT]
     context = {
         'group': group,
         'posts': posts,
